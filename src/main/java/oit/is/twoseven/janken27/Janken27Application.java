80c87f96500a1a4ccc440f18b1000e8c011727d3
e1b18031 <e1b18031@st.oit.ac.jp>
2020-10-06 16:10:45 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/twoseven/janken27/Janken27Application.java
@@ -0,0 +1,13 @@
+package oit.is.twoseven.janken27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Janken27Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Janken27Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/twoseven/janken27/Janken27ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.twoseven.janken27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Janken27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
